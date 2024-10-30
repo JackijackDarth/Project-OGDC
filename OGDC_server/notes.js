6f86188a3d396fc18e80c490479ab090e39a0b50
MathLecher <150386983+MathLecher@users.noreply.github.com>
2024-10-30 00:12:57 -0400

    start notes

---
+++ OGDC_server/notes.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const notesFilePath = "./BD/listeNotes.json";
+
+//TODO
+//FONCTION NOTE
+
+
+
+module.exports = {
+   
+};
--- OGDC_server/routes/notes.js
+++ OGDC_server/routes/notes.js
@@ -1,23 +1,46 @@
 const fs = require('fs');
 const { Router } = require('express');
-const user = require('../users');
+const notes = require('../notes');
 
 const notesRoutes = Router();
 
-/**
- * deconnexionRoutes /GET/ idUser/
- * Déconnexion de l'usager connecter
- * Retourne 201 + user | 401
- */
+
 notesRoutes.route('/')
-    .get((req, res) => {
-        console.log("Déconnexion l'usager ", req.params.idUser);
-        const resultat = user.Deconnexion(req.params.idUser);
+    .post((req, res) => {
+        console.log("Création note  ");
+        const resultat = notes.CreerNotes(req.body);
         if (resultat.erreur !== 0) {
             res.status(401).send(resultat);
         } else {
-            res.json(resultat.user);
+            res.status(201);
         }
     });
+
+notesRoutes.route('/:method/:param1')
+    .delete((req,res)=>{
+        if(req.params.method == "delete"){
+            console.log("Commande delete : ", req.params.param1);
+            let resultat = notes.SupprimerNote(req.params.param1);
+            console.log(resultat);
+            if (resultat.erreur !== 0) {
+                res.status(401).send(resultat);
+            } else {
+                res.status(201).send(resultat);
+            }
+        }
+    })
+    .get((req,res)=>{
+        if(req.params.method == "get"){
+            console.log("Obtenir les notes pour la famille : ",req.params.param1);
+            const resultat = notes.ObtenirNotesFamille(req.params.param1);
+            console.log("Résultat de obtenirTouteCommandes:", resultat);
+            
+            if (resultat.erreur !== 0) {
+                res.status(401).send(resultat);
+            } else {
+                res.json(resultat.notes);
+            }
+        }
+    })
 
 module.exports = notesRoutes;
